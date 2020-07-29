89e875c9e00be085842758b5c1d03227d45d6b1c
Amin <amin.fokrul@nttdata.com>
2020-07-29 19:45:21 +0100

    first commit

---
+++ algorithm-practise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithm_practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
